198ad2d96f65fe09a47c6dd8520b739b57322590
Eckspur <185216831+aerobis@users.noreply.github.com>
2026-08-19 18:02:03 +0930

    Standardized raw data's columns. Added a description for Data Wrangling.

--- scripts/main.py
+++ scripts/main.py
@@ -18,3 +18,24 @@
 # Null Hypotehsis (H0): μ_qualified = μ_eliminated
 # Alternate Hypothesis (h1): μ_qualified > μ_eliminated
 # ============================================================
+
+# IMPORTING LIBRARIES
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+# ============================================================
+# 2. DATA WRANGLING
+# ============================================================
+# Data Source:
+#   - The Stats Don't Lie
+#   - FBref World Cup Stats
+#   - Official Fifa Site
+#
+# Data Description:
+# Raw dataset contains 48 teams with the following columns:
+# TEAM | GROUP_STAGE_MATCHES | GROUP_STAGE_GOALS | QUALIFIED
+# 
+# Data has been manually collected and stored in MsExcel.
+# ============================================================
