7682124d6f895e8a8f866fd6a2e2d6f9f1cd4aba
Eckspur <185216831+aerobis@users.noreply.github.com>
2026-08-19 17:54:53 +0930

    Added basic introduction to Task 1 in the main python file.

---
+++ scripts/main.py
@@ -0,0 +1,20 @@
+# ============================================================
+# FIFA WORLD CUP 2026 DATA ANALYSIS
+# TASK 1
+# ============================================================
+
+# ============================================================
+# 1. Analytic Question Formulation
+# ============================================================
+# Research Question:
+# Do teams that qualify for the Knockout Stage score significantly more
+# goals on average than the teams that are eliminated in the Group Stage?
+#
+# Variables:
+# Independent Variable   ->      Qualification Status (Yes or No)
+# Dependent Variable     ->      Goal on Average (Goal per match) for Group Stage
+#
+# Hypotheses:
+# Null Hypotehsis (H0): μ_qualified = μ_eliminated
+# Alternate Hypothesis (h1): μ_qualified > μ_eliminated
+# ============================================================
